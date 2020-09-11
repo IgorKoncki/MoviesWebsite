ef70afcb110c36b8eec7edbc1aaef0843aa3a995
igork <igorkoncki@gmail.com>
2020-09-11 12:26:29 +0200

    Dodaj pliki projektów.

---
+++ Cwiczenia9/Data/Cwiczenia9Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cwiczenia9.Models
+{
+    public class Cwiczenia9Context : DbContext
+    {
+        public Cwiczenia9Context (DbContextOptions<Cwiczenia9Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cwiczenia9.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Cwiczenia9/Models/MovieGenreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Cwiczenia9.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public SelectList Ratings { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+        public string Rating { get; set; }
+    }
+}
